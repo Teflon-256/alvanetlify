3f6640af2c4f0854bd55b5c90257c59986744803
Teflon-256 <sahabyoona@gmail.com>
2025-09-25 17:26:38 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './client/src'),
       '@assets': path.resolve(__dirname, './client/src/assets'),
+      '@generated_images': path.resolve(__dirname, './client/src/generated_images'),
     },
   },
   build: {
