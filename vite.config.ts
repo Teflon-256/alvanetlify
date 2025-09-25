087b95c8e80aa949218527e34918dabcbfb25fe1
Teflon-256 <sahabyoona@gmail.com>
2025-09-25 09:18:22 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
-import cartographer from '@replit/vite-plugin-cartographer';
-import runtimeErrorModal from '@replit/vite-plugin-runtime-error-modal';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cartographer(), runtimeErrorModal(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
       external: ['axios']
